bb7865635c8a421ca8bcce1f8c36c42e15ed2ea7
krieger27 <krieger@vols.utk.edu>
2018-02-01 15:29:46 -0600

    getboardsandaddboards

--- js/factories/PinFactory.js
+++ js/factories/PinFactory.js
@@ -1,5 +1,32 @@
 'use strict';
 
-module.exports = function () {
+const firebase = require('firebase');
 
-};
+module.exports = function($q, $http, FBUrl) {
+    const getBoards = (uid) => {
+        return $q((resolve, reject) => {
+            $http
+            .get(`${FBUrl}`)
+            .then(({data}) => {
+                resolve(data);
+            });
+        });
+    };
+
+    function addBoards() {
+        return $q((resolve, reject) => {
+            $http
+                .post(`${FBUrl}/boards.json?orderBy="uid"&equalTo="${firebase.auth().currentUser.uid}"`);
+                JSON.stringify(addBoards)
+                .then(({ data }) => {
+                    let boardArr = Object.keys(data).map(boardKey => {
+                        console.log('boardKey', boardKey);
+                        data[boardKey].id = boardKey;
+                        return data[boardKey];
+                    });
+                });
+        });
+    }
+        return {addBoards, getBoards};
+    };
+    
