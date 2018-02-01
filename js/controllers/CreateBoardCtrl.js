0267b9723cfbf866cadf1d63549d0341a68e5171
krieger27 <krieger@vols.utk.edu>
2018-02-01 15:28:51 -0600

    saveboard

--- js/controllers/CreateBoardCtrl.js
+++ js/controllers/CreateBoardCtrl.js
@@ -1,5 +1,26 @@
 'use strict';
 
-module.exports = function ($scope) {
+const firebase = require('firebase');
 
+module.exports = function($scope, PinFactory){
+    PinFactory.getBoards()
+    .then(boards => {
+        $scope.boards = boards;
+    });
+};
+
+module.exports =function($scope, $location, PinFactory) {
+    $scope.title = "New";
+    $scope.board = {
+        title: ''
+    };
+
+    $scope.saveBoard = () => {
+        console.log('New Board to add', $scope.board );
+        $scope.board.uid = firebase.auth().currentUser.uid;
+        PinFactory.addBoard($scope.board)
+        .then( (data) => {
+        $location.url("/boards/new");
+        });
+    };
 };
