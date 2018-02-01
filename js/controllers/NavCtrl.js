6ecde00343d45f6f59b78c1885594f746013a051
Tim A. Conner <timaconner1@gmail.com>
2018-02-01 14:51:35 -0600

    Add navbar partial and check if logged in.

--- js/controllers/NavCtrl.js
+++ js/controllers/NavCtrl.js
@@ -1,5 +1,38 @@
 'use strict';
 
-module.exports = function ($scope) {
+const firebase = require('firebase');
+
+module.exports = function ($scope, AuthFactory, $window) {
+    $scope.navMenuList = [
+        {
+            text: "Add Board",
+            url: "#!/createboard"
+        },
+        {
+            text: "Add Pin",
+            url: "#!/createpin"
+        },
+        {
+            text: "My Boards",
+            url: "#!/boards"
+        },
+        {
+            text: "Login",
+            url: "#!/login",
+            hideWhenLoggedIn: true
+        },
+        {
+            text: "Logout",
+            url: "#!/logout"
+        }
+    ];
+
+    // If the url is #!/logout, then logout, otherwise go to url.
+    $scope.navigate = url => {
+        if (url === "#!/logout") AuthFactory.logoutUser();
+        else $window.location.href = url;
+    };
+
+    $scope.isLoggedIn = () => firebase.auth().currentUser;
 
 };
