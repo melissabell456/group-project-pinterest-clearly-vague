e2908a2966a7b4aaaebc90529479105537c913c9
Melissa Bell <melissabell456@gmail.com>
2018-02-02 13:33:24 -0600

    refactored deconstruction of userObj from firebase for displayName and uid

--- js/factories/AuthFactory.js
+++ js/factories/AuthFactory.js
@@ -10,14 +10,11 @@ module.exports = function (FBCreds, $q, $window, $http, FBUrl) {
     const loginUser = () => {
         firebase.auth().signInWithPopup(provider)
         // deconstructing user object to access full name, uid(G), and whether or not user is new (isNewUser)
-            .then( ({additionalUserInfo: {isNewUser}, user: {displayName, G}}) => { 
+            .then( ({additionalUserInfo: {isNewUser}, user: {displayName: name, G: uid}}) => { 
         // utilizing property on firebase authentication to check if user is new
                 if (isNewUser) {
         // gathering new user's name and uid to send to firebase user collection
-                    let userDetails = {
-                        name: displayName,
-                        uid: G,
-                    };
+                    let userDetails = {name, uid};
                     addUserToDB(userDetails);
                 }
                 $window.location.href = "#!/boards";
