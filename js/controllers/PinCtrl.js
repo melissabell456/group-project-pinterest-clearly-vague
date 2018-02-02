def2d2089af1ad2b84fdf761333492f363941199
Tim A. Conner <timaconner1@gmail.com>
2018-02-02 10:24:08 -0600

    Add: function on PinFctry to accept BoardId.

--- js/controllers/PinCtrl.js
+++ js/controllers/PinCtrl.js
@@ -1,5 +1,13 @@
 'use strict';
 
-module.exports = function ($scope) {
-
+module.exports = function ($scope, $routeParams, PinFactory) {
+    PinFactory.getPins($routeParams.boardID)
+    .then(pins => {
+        // Add firebase key to each pin.
+        let pinArray = Object.keys(pins).map(key => {
+            pins[key].id = key;
+            return pins[key];
+        });
+        $scope.pinArray = pinArray;
+    });
 };
