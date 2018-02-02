5ac3fa5f8c92e55c05dea5ff03212f526b1b66e0
Melissa Bell <melissabell456@gmail.com>
2018-02-02 10:42:31 -0600

    commit prior to checking out trey's brach per git req

--- js/factories/AuthFactory.js
+++ js/factories/AuthFactory.js
@@ -2,28 +2,39 @@
 
 const firebase = require('firebase');
 
-module.exports = function (FBCreds, $q, $window, $http) {
+module.exports = function (FBCreds, $q, $window, $http, FBUrl) {
 
     let provider = new firebase.auth.GoogleAuthProvider();
     let currentUser = null;
 
     const loginUser = () => {
         firebase.auth().signInWithPopup(provider)
-            .then(({ user }) => {
+            .then((user) => {
                 console.log("logged in!", user);
+                let userDetails = {
+                    name: user.displayName,
+                    uid: user.G,
+                };
+                addUserToDB(userDetails);
                 // $window.location.href = "#!/boards";
             })
             .catch(error => console.log('error', error));
     };
 
     const addUserToDB = (userObj) => {
-        return $q( (resolve, reject) => {
-            $http
-            .post(`${FBUrl}/users.json`, 
-            JSON.stringify(userObj))
-            .then( (data) => {
-                console.log(data);
-            })
+        // console.log(userObj);
+        // return $q( (resolve, reject) => {
+        //     $http
+        //     .post(`${FBUrl}/users.json`, 
+        //     JSON.stringify(userObj))
+        //     .then( (data) => {
+        //         console.log(data);
+        //     })
+        //     .catch( (err) => {
+        //         console.log(err);
+        //     });
+        // });
+    };
             
 
     const logoutUser = () => {
