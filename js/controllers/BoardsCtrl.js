8513d22893777194fb32a6f994ddd40693248ede
krieger27 <krieger@vols.utk.edu>
2018-02-02 14:03:17 -0600

    deletefnc

--- js/controllers/BoardsCtrl.js
+++ js/controllers/BoardsCtrl.js
@@ -3,8 +3,17 @@
 const firebase = require('firebase');
 
 module.exports = function($scope, PinFactory){
+
     PinFactory.getBoards()
     .then(boards => {
         $scope.boards = boards.data;
     });
+
+    $scope.deleteBoard = (boardId) => {
+        PinFactory.deleteBoards(boardId)
+        .then(boards => {
+            $scope.boards = boards.data;
+        });
+    };
 };
+
